a1914b8d20fb54a0e532825d65f39da83337d021
Michael Chavez <bigbassroller@gmail.com>
2016-03-18 13:56:24 -0700

    add other pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import Application from './lib';
 import HelloController from './HelloController';
 import HomeController from './components/pages/Home/HomeController';
 import AboutController from './components/pages/About/AboutController';
-
+import PortfolioController from './components/pages/Portfolio/PortfolioController';
+import BlogController from './components/pages/Blog/BlogController';
+import ContactController from './components/pages/Contact/ContactController';
 import nunjucks from 'nunjucks';
 import options from './options';
 
@@ -11,7 +13,10 @@ nunjucks.configure(options.nunjucks);
 const application = new Application({
   '/hello/{name*}': HelloController,
   '/': HomeController,
-  '/about': AboutController
+  '/about': AboutController,
+  '/portfolio': PortfolioController,
+  '/blog': BlogController,
+  '/contact': ContactController,
 }, options);
 
 application.start();
