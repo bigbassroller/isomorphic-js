8762b492c4c9fb6ce64a977c4445b157f5983e85
Michael Chavez <bigbassroller@gmail.com>
2016-03-27 03:24:44 -0700

    made vannilla style

--- src/components/pages/Home/HomeController.js
+++ src/components/pages/Home/HomeController.js
@@ -1,5 +1,6 @@
 import Controller from '../../../lib/Controller';
 import nunjucks from 'nunjucks';
+import Trianlify from 'trianglify';
 
 function onClick(e) {
   console.log(e.currentTarget);
@@ -8,41 +9,21 @@ function onClick(e) {
 function ignitor(context) {
   // default values
   let name = {
-    fname: 'Michael',
-    lname: 'Chavez',
     cloud1: '/cloud-1.png',
     cloud2: '/cloud-2.png',
     cloud3: '/cloud-3.png'
   };
-  // split path params
-  let nameParts = context.params.name ? context.params.name.split('/') : [];
-
-  // order of precedence
-  // 1. path param
-  // 2. query param
-  // 3. default value
-  name.fname = (nameParts[0] || context.query.fname) ||
-    name.fname;
-  name.lname = (nameParts[1] || context.query.lname) ||
-    name.lname;
 
   return name;
 }
 
 export default class HomeController extends Controller {
 
-  index(application, request, reply, callback) {
-    this.context.cookie.set('random', '_' + (Math.floor(Math.random() * 1000) + 1), { path: '/' });
-    this.context.data = { random: Math.floor(Math.random() * 1000) + 1 };
-    callback(null);
-  }
-
   toString(callback) {
     // this can be handled more eloquently using Object.assign
     // but we are not including the polyfill dependency
     // for the sake of simplicity
     let context = ignitor(this.context);
-    context.data = this.context.data;
 
     nunjucks.render('components/pages/Home/home.html', context, (err, html) => {
       if (err) {
