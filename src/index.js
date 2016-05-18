bf709305579ee6314d07e73c64bce49e379b9ffe
Michael Chavez <bigbassroller@gmail.com>
2016-05-18 08:37:01 -0700

    added blog route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Application from './lib';
 import HelloController from './components/pages/Hello/HelloController';
 import HomeController from './components/pages/Home/HomeController';
 import AboutController from './components/pages/About/AboutController';
+import BlogController from './components/pages/Blog/BlogController';
 import nunjucks from 'nunjucks';
 import options from './options';
 
@@ -10,7 +11,8 @@ nunjucks.configure(options.nunjucks);
 const application = new Application({
   '/hello/{name*}': HelloController,
   '/': HomeController,
-  '/about': AboutController
+  '/about': AboutController,
+  '/blog': BlogController
 }, options);
 
 application.start();
